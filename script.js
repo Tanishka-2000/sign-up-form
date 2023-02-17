46cc4a3b1040d5df29cf00a80761711c14a0d4d2
Shashank Talwar <shashanktalwar81@gmail.com>
2023-02-17 11:51:08 +0530

    add image

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let password = document.querySelector("#password");
 let passwordReTyped = document.querySelector("#confirmPassword");
 let errorMessage = document.querySelector(".error_message");
 
+// matching password fields
 passwordReTyped.addEventListener("input",function(){
     if(password.value === this.value){
         errorMessage.style.display = "none";
